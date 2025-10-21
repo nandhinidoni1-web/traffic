d1b3bc51cceac8a364c755a571181f5ff46774e9
nandhinidoni1-web <nandhinidoni1@gmail.com>
2025-10-21 22:05:56 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0a1a2a',
+        neonRed: '#ff073a',
+        electricCyan: '#00e5ff',
+        accentWhite: '#ffffff'
+      },
+    },
+  },
+  plugins: [],
+};
